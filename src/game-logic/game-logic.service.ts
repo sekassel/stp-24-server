fb164a1867001619cae5f9fc2eec36d6d7251c2b
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-27 12:29:10 +0100

    feat: Add district and building resource upkeep and production

--- src/game-logic/game-logic.service.ts
+++ src/game-logic/game-logic.service.ts
@@ -7,6 +7,10 @@ import {EmpireDocument} from '../empire/empire.schema';
 import {SystemDocument} from '../system/system.schema';
 import {calculateVariables, getInitialVariables} from './variables';
 import {Variable} from './types';
+import {ResourceName} from './resources';
+import {DistrictName, DISTRICTS} from './districts';
+import {EMPIRE_VARIABLES} from './empire-variables';
+import {BUILDINGS} from './buildings';
 
 @Injectable()
 export class GameLogicService {
@@ -45,10 +49,71 @@ export class GameLogicService {
     const popUpkeep = variables['empire.pop.consumption.food'] * empire.resources.population;
     empire.resources.food = Math.max(0, empire.resources.food - popUpkeep);
 
-    // handle districts
+    // handle districts and buildings
     for (const system of systems) {
-      for (const [districtType, count] of Object.entries(system.districts)) {
+      let upgrade: 'colonized' | 'upgraded' | 'developed';
+      switch (system.upgrade) {
+        case 2:
+          upgrade = 'colonized';
+          break;
+        case 3:
+          upgrade = 'upgraded';
+          break;
+        case 4:
+          upgrade = 'developed';
+          break;
+        default:
+          continue;
+      }
 
+      const systemUpkeepPaid = this.deductSystemUpkeep(upgrade, empire, variables);
+      if (!systemUpkeepPaid) {
+        continue;
+      }
+
+      const jobs = Object.values(system.districts).sum() + system.buildings.length;
+      const popCoverage = Math.clamp(system.population / jobs, 0, 1);
+
+      for (const [districtType, count] of Object.entries(system.districts)) {
+        const district = DISTRICTS[districtType as DistrictName];
+
+        // deduct district upkeep
+        let districtUpkeepPaid = true;
+        for (const resource of Object.keys(district.upkeep)) {
+          const variable = `districts.${districtType}.upkeep.${resource}` as Variable;
+          districtUpkeepPaid = this.deductResource(empire, resource as ResourceName, variables[variable] * count * popCoverage) && districtUpkeepPaid;
+        }
+
+        if (!districtUpkeepPaid) {
+          continue;
+        }
+
+        // add district production
+        for (const resource of Object.keys(district.production)) {
+          const variable = `districts.${districtType}.production.${resource}` as Variable;
+          empire.resources[resource as ResourceName] += count * variables[variable] * popCoverage;
+        }
+      }
+
+      for (const buildingType of system.buildings) {
+        const building = BUILDINGS[buildingType];
+
+        // deduct building upkeep
+        let buildingUpkeepPaid = true;
+        for (const resource of Object.keys(building.upkeep)) {
+          const variable = `buildings.${buildingType}.upkeep.${resource}` as Variable;
+          buildingUpkeepPaid = this.deductResource(empire, resource as ResourceName, variables[variable] * popCoverage) && buildingUpkeepPaid;
+        }
+
+        if (!buildingUpkeepPaid) {
+          continue;
+        }
+
+        // add building production
+        for (const resource of Object.keys(building.production)) {
+          const variable = `buildings.${buildingType}.production.${resource}` as Variable;
+          empire.resources[resource as ResourceName] += variables[variable] * popCoverage;
+        }
       }
     }
 
@@ -68,6 +133,25 @@ export class GameLogicService {
     if (empire.resources.food) {
       this.popGrowth(systems, variables);
       empire.resources.population = systems.map(s => s.population).sum();
+    }
+  }
+
+  private deductSystemUpkeep(upgrade: 'colonized' | 'upgraded' | 'developed', empire: EmpireDocument, variables: Record<Variable, number>) {
+    let systemUpkeepPaid = true;
+    for (const resource of Object.keys(EMPIRE_VARIABLES.system[upgrade].consumption)) {
+      const variable = `empire.system.${upgrade}.consumption.${resource}` as Variable;
+      systemUpkeepPaid = this.deductResource(empire, resource as ResourceName, variables[variable]) && systemUpkeepPaid;
+    }
+    return systemUpkeepPaid;
+  }
+
+  private deductResource(empire: EmpireDocument, resource: ResourceName, amount: number) {
+    if (empire.resources[resource] < amount) {
+      empire.resources[resource] = 0;
+      return false;
+    } else {
+      empire.resources[resource] -= amount;
+      return true;
     }
   }
 
