ca686e1267806c03119a4db5fc57df51365b7f57
Adrian Kunz <clashsoft@hotmail.com>
2024-03-17 22:31:28 +0100

    feat: System type ids

--- src/game-logic/system-types.ts
+++ src/game-logic/system-types.ts
@@ -1,35 +1,42 @@
 export const SYSTEM_TYPES = {
   regular: {
+    id: 'regular',
     chance: 10,
     capacity_range: [10, 25],
     district_percentage: 0.8,
   },
   energy: {
+    id: 'energy',
     chance: 3,
     capacity_range: [12, 25],
     district_percentage: 0.8,
   },
   mining: {
+    id: 'mining',
     chance: 3,
     capacity_range: [13, 28],
     district_percentage: 0.8,
   },
   agriculture: {
+    id: 'agriculture',
     chance: 3,
     capacity_range: [15, 30],
     district_percentage: 0.8,
   },
   ancient_technology: {
+    id: 'ancient_technology',
     chance: 1,
     capacity_range: [10, 18],
     district_percentage: 0.8,
   },
   ancient_industry: {
+    id: 'ancient_industry',
     chance: 1,
     capacity_range: [10, 20],
     district_percentage: 0.8,
   },
   ancient_military: {
+    id: 'ancient_military',
     chance: 1,
     capacity_range: [10, 16],
     district_percentage: 0.8,
