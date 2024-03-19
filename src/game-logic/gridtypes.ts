68c2939249ea6e2a52ad7994bb1136e7b18d0437
Giulio <74862175+Giulcoo@users.noreply.github.com>
2024-03-19 14:42:00 +0100

    Multiple Cluster Generation (#30)
    * feat: single cluster creation
    
    * fix: creating systems and linking them
    
    * feat: creating regular type systems
    
    * fix: spanning tree remove visited
    
    * feat: better db communication
    
    * feat: cleaner code and removed comments
    
    * feat: grid system for cluster generation
    
    * feat: started generateDistricts
    
    * feat: district generation
    
    * feat: upgrade costs
    
    * feat: weighted random
    
    * fix: Runtime errors
    
    * feat: randomWeighted with mapping
    
    * fix: correct index in randomWeighted
    
    * feat: cost resource check
    
    * fix: system type depending upgrades
    
    * feat: saving changed empire
    
    * feat: added missing await
    
    * feat: alternative grid system
    
    * feat: new grid system
    
    * feat: multiple clusters
    
    * feat: multiple clusters in circle
    
    * feat: parameter tuning
    
    * small fixes
    
    ---------
    
    Co-authored-by: Adrian Kunz <a.kunz@uni-kassel.de>

--- src/game-logic/gridtypes.ts
+++ src/game-logic/gridtypes.ts
@@ -1,55 +1,72 @@
-export const GRIDS: number[][][] = [
-  [
-    [0,1],[0,2],
-    [1,0],[1,1],[1,2],[1,3],
-    [2,0],[2,1],[2,2],[2,3],
-    [3,1],[3,2],
-  ]
-] as const;
+export class Grid {
+  vertices: readonly Vertex[];
+  intersecting_edges: readonly number[][][];
+  system_range: readonly [number, number];
+  cycle_percentage: number;
+}
 
-export const GRIDS_NEW  = [
-  [
-    {
-      vertices: [
-        { id: 0, x: 1, y: 0, neighbors: [1,2,3,4] },
-        { id: 1, x: 2, y: 0, neighbors: [0,3,4,5] },
-        { id: 2, x: 0, y: 1, neighbors: [0,3,6,7] },
-        { id: 3, x: 1, y: 1, neighbors: [0,1,2,4,6,7,8] },
-        { id: 4, x: 2, y: 1, neighbors: [0,1,3,5,7,8,9] },
-        { id: 5, x: 3, y: 1, neighbors: [1,4,8,9] },
-        { id: 6, x: 0, y: 2, neighbors: [2,3,7,10] },
-        { id: 7, x: 1, y: 2, neighbors: [2,3,4,6,8,10,11] },
-        { id: 8, x: 2, y: 2, neighbors: [3,4,5,7,8,10,11] },
-        { id: 9, x: 3, y: 2, neighbors: [4,5,8,11] },
-        { id: 10, x: 1, y: 3, neighbors: [6,7,8,11] },
-        { id: 11, x: 2, y: 3, neighbors: [7,8,9,10] },
-      ],
-      intersecting_edges: [
-        [[0,4], [1,3]],
-        [[2,7], [3,6]],
-        [[3,8], [4,7]],
-        [[4,9], [5,8]],
-        [[7,11], [8,10]],
-      ]
-    },
-    {
-      vertices: [
-        { id: 0, x: 2, y: 0, neighbors: [1,2,4] },
-        { id: 1, x: 1, y: 1, neighbors: [0,2,3,4,6] },
-        { id: 2, x: 3, y: 1, neighbors: [0,1,4,5,7] },
-        { id: 3, x: 0, y: 2, neighbors: [1,4,6] },
-        { id: 4, x: 2, y: 2, neighbors: [0,1,2,3,5,6,7,8] },
-        { id: 5, x: 4, y: 2, neighbors: [2,4,7] },
-        { id: 6, x: 1, y: 3, neighbors: [1,3,4,7,8] },
-        { id: 7, x: 3, y: 3, neighbors: [2,4,5,6,8] },
-        { id: 8, x: 2, y: 4, neighbors: [4,6,7] },
-      ],
-      intersecting_edges: [
-        [[0,4], [1,2]],
-        [[1,6], [3,4]],
-        [[2,7], [4,5]],
-        [[4,8], [6,7]],
-      ]
-    },
-  ]
-] as const;
+export class Vertex {
+  id: number;
+  x: number;
+  y: number;
+  neighbors: readonly number[];
+}
+
+export const GRIDS  = [
+  {
+    vertices: [
+      { id: 0, x: 1, y: 0, neighbors: [1,2,3,4] },
+      { id: 1, x: 2, y: 0, neighbors: [0,3,4,5] },
+      { id: 2, x: 0, y: 1, neighbors: [0,3,6,7] },
+      { id: 3, x: 1, y: 1, neighbors: [0,1,2,4,6,7,8] },
+      { id: 4, x: 2, y: 1, neighbors: [0,1,3,5,7,8,9] },
+      { id: 5, x: 3, y: 1, neighbors: [1,4,8,9] },
+      { id: 6, x: 0, y: 2, neighbors: [2,3,7,10] },
+      { id: 7, x: 1, y: 2, neighbors: [2,3,4,6,8,10,11] },
+      { id: 8, x: 2, y: 2, neighbors: [3,4,5,7,8,10,11] },
+      { id: 9, x: 3, y: 2, neighbors: [4,5,8,11] },
+      { id: 10, x: 1, y: 3, neighbors: [6,7,8,11] },
+      { id: 11, x: 2, y: 3, neighbors: [7,8,9,10] },
+    ],
+    intersecting_edges: [
+      [[0,4], [1,3]],
+      [[2,7], [3,6]],
+      [[3,8], [4,7]],
+      [[4,9], [5,8]],
+      [[7,11], [8,10]],
+    ],
+    system_range: [9,12],
+    cycle_percentage: 0.5,
+  },
+  {
+    vertices: [
+      { id: 0, x: 2, y: 0, neighbors: [1,2,4] },
+      { id: 1, x: 1, y: 1, neighbors: [0,2,3,4,6] },
+      { id: 2, x: 3, y: 1, neighbors: [0,1,4,5,7] },
+      { id: 3, x: 0, y: 2, neighbors: [1,4,6] },
+      { id: 4, x: 2, y: 2, neighbors: [0,1,2,3,5,6,7,8] },
+      { id: 5, x: 4, y: 2, neighbors: [2,4,7] },
+      { id: 6, x: 1, y: 3, neighbors: [1,3,4,7,8] },
+      { id: 7, x: 3, y: 3, neighbors: [2,4,5,6,8] },
+      { id: 8, x: 2, y: 4, neighbors: [4,6,7] },
+    ],
+    intersecting_edges: [
+      [[0,4], [1,2]],
+      [[1,6], [3,4]],
+      [[2,7], [4,5]],
+      [[4,8], [6,7]],
+    ],
+    system_range: [9,11],
+    cycle_percentage: 0.5,
+  },
+] as const satisfies Grid[];
+
+export const GRID_SCALING = 12;
+export const MAX_SYSTEM_DISPLACEMENT = 0.45;
+
+export const CIRCLE_GENERATOR = {
+  radius_steps: 0.01,
+  angle_steps: 300,
+  radius_angle_percentage: 1,
+  collision_precision: 0.99,
+}
--- src/system/system.service.ts
+++ src/system/system.service.ts
@@ -4,7 +4,7 @@ import {Model, Types} from 'mongoose';
 import {EventRepository, EventService, MongooseRepository} from '@mean-stream/nestx';
 import {System,SystemDocument} from './system.schema';
 import {Game} from "../game/game.schema";
-import {GRIDS} from "../game-logic/gridtypes";
+import {CIRCLE_GENERATOR, GRID_SCALING, GRIDS, Grid, Vertex, MAX_SYSTEM_DISPLACEMENT} from "../game-logic/gridtypes";
 import {UpdateSystemDto} from './system.dto';
 import {
   SYSTEM_UPGRADE_NAMES,
@@ -143,118 +143,204 @@ export class SystemService extends MongooseRepository<System> {
     }
 
     const clusters: System[][] = [];
+    const clustersCenter: number[][] = [];
+    const clustersRadius: number[] = [];
+    let avgRadius = -1;
 
-    //TODO: Generate multiple clusters
-    clusters.push(this.createCluster(game, 15, [-40, -40]));
+    //Create clusters
+    while(clusters.flat().length < game.settings.size){
+      const cluster = this.createCluster(game, GRID_SCALING, [-GRID_SCALING*2,-GRID_SCALING*2]);
+      const center = this.calcClusterCenter(cluster);
+      const radius = this.calcClusterRadius(cluster, center);
 
-    //TODO: Connect clusters
+      clusters.push(cluster);
+      clustersCenter.push(center);
+      clustersRadius.push(radius);
+
+      if (avgRadius === -1) {
+        avgRadius = radius;
+      }
+      else {
+        avgRadius = (avgRadius + radius) / 2;
+      }
+    }
+
+
+    //Spread clusters across the map
+    for(let i = 1; i < clusters.length; i++){
+      let angle = 0;
+      let angleOffset = Math.PI*2*Math.random()
+      let radius = avgRadius;
+
+      while(this.hasClusterCollision(clustersCenter, clustersRadius, i)){
+        angle += Math.PI/(radius * CIRCLE_GENERATOR.radius_angle_percentage + CIRCLE_GENERATOR.angle_steps);
+
+        if(angle > Math.PI*2){
+          angle = 0;
+          angleOffset = Math.PI*2*Math.random();
+          radius += avgRadius * CIRCLE_GENERATOR.radius_steps;
+        }
+
+        const movement = [Math.cos(angle + angleOffset)*radius, Math.sin(angle + angleOffset)*radius];
+        clusters[i] = this.moveCluster(clusters[i], movement);
+        clustersCenter[i] = [clustersCenter[i][0] + movement[0], clustersCenter[i][1] + movement[1]];
+      }
+    }
+
+    //Connect clusters
+
 
     return this.createMany(clusters.flat());
+  }
+
+  private moveCluster(cluster: System[], movement: number[]): System[] {
+    return cluster.map(system => {
+      system.x += movement[0];
+      system.y += movement[1];
+      return system;
+    });
+  }
+
+  private hasClusterCollision(clusterCenters: number[][], clusterRadius: number[], clusterIndex: number): boolean {
+    for(let i = 0; i < clusterCenters.length; i++){
+      if(i === clusterIndex) continue;
+      if(this.clusterCollision(clusterCenters[i], clusterCenters[clusterIndex],
+        clusterRadius[i]*CIRCLE_GENERATOR["collision_precision"],
+        clusterRadius[clusterIndex]*CIRCLE_GENERATOR["collision_precision"])) return true;
+    }
+    return false;
+  }
+
+  private clusterCollision(center1: number[], center2: number[], radius1: number, radius2: number): boolean {
+    return Math.hypot(center1[0] - center2[0], center1[1] - center2[1]) < radius1 + radius2;
   }
 
   /**
    * Creates a cluster of systems and connects these systems
    */
-  // private createCluster(game: Game, count: number, radius: number, center: number[]): System[] {
   private createCluster(game: Game, scaling: number, offset: number[]): System[] {
-    const systems: System[] = [];
+    const grid:Grid = GRIDS[Math.randInt(GRIDS.length)];
+    const systemAmount = Math.randInt(grid.system_range[1] - grid.system_range[0]) + grid.system_range[0];
+    const vertices: number[] = Array.from(grid.vertices.map(vertex => vertex.id)).sort(() => Math.random() - 0.5).slice(0, systemAmount);
+    const edges: number[][] = this.createSpanningTree(grid, vertices);
+
+    //Add random cycles
+    const randomCycles = vertices.length * grid.cycle_percentage;
+    for(let i = 0; i < randomCycles; i++) {
+      const system1 = vertices[Math.randInt(vertices.length)];
+      const neighbors = grid.vertices[system1].neighbors.filter(neighbor => vertices.includes(neighbor));
+      const system2 = neighbors[Math.randInt(neighbors.length)];
+      const newEdge = Array.from([system1, system2]).sort(v => v);
+
+      if(!edges.includes(newEdge) && !this.hasIntersection(grid, edges, newEdge)){
+        edges.push([system1, system2]);
+      }
+    }
 
     //Create systems
-    const grid:number[][] = GRIDS[Math.randInt(GRIDS.length)].sort(() => Math.random() - 0.5).slice(0, 10);
+    const systems: Record<number, System> = {};
+    vertices.forEach(vertex => systems[vertex] = this.createSystem(game, grid.vertices[vertex], scaling, offset));
 
-    for(const [x, y] of grid) {
-      systems.push(this.createSystem(game, (x + Math.random()*0.25) * scaling + offset[0], (y + Math.random()*0.25) * scaling + offset[1]));
+    //Connect systems
+    for(const [system1, system2] of edges) {
+      this.connectSystems(systems[system1], systems[system2]);
     }
 
-    //Connect systems as a spanning tree
-    const visitedSystems: System[] = [systems[0]];
-    while(visitedSystems.length < systems.length) {
-      let nextSystem: System | undefined = undefined;
-      for(const system of this.edgesSortedSystems(visitedSystems)){
-        if(Math.random() < 0.3){
-          nextSystem = system;
-          break;
-        }
+
+    return Object.values(systems);
+  }
+
+  private createSpanningTree(grid: Grid, vertices: number[]): number[][] {
+    const edges: number[][] = [];
+
+    const visited = [vertices[Math.randInt(vertices.length)]];
+
+    while(visited.length < vertices.length) {
+      const candidateEdges = [];
+
+      for(const vertex of visited) {
+        const validNeighborEdges = grid.vertices[vertex].neighbors
+          .filter(neighbor => vertices.includes(neighbor) && !visited.includes(neighbor))
+          .map(neighbor => vertex > neighbor ? [neighbor, vertex] : [vertex, neighbor])
+          .filter(edge => !this.hasCycle(vertex, edges) && !this.hasIntersection(grid, edges, edge));
+
+        candidateEdges.push(...validNeighborEdges);
       }
 
-      if(!nextSystem) nextSystem = visitedSystems[Math.randInt(visitedSystems.length)];
+      const newEdge = candidateEdges[Math.randInt(candidateEdges.length)];
+      edges.push(newEdge);
+      visited.push(newEdge.find(vertex => !visited.includes(vertex))!);
+    }
 
-      const candidates = this.distanceSortedSystems(systems.filter(s => !visitedSystems.includes(s)), nextSystem);
+    return edges;
+  }
 
-      for(const otherSystem of candidates) {
-        this.connectSystems(nextSystem, otherSystem);
+  private calcClusterCenter(cluster: System[]): number[] {
+    const x = cluster.reduce((acc, system) => acc + system.x, 0) / cluster.length;
+    const y = cluster.reduce((acc, system) => acc + system.y, 0) / cluster.length;
+    return [x, y];
+  }
 
-        if(this.checkForCycles(systems, nextSystem)) {
-          this.removeConnection(nextSystem, otherSystem);
-        } else{
-          visitedSystems.push(otherSystem);
-          break;
+  private calcClusterRadius(cluster: System[], center: number[]): number {
+    return cluster.reduce((acc, system) => Math.max(acc, Math.hypot(system.x - center[0], system.y - center[1])), 0);
+  }
+
+  private connectCluster(cluster1: System[], cluster2: System[]) {
+    let nearestSystems: System[] = [];
+    let nearesSystemDistance = -1;
+
+    for(const system1 of cluster1){
+      for(const system2 of cluster2){
+        const distance = Math.hypot(system1.x - system2.x, system1.y - system2.y);
+        if(nearesSystemDistance === -1 || distance < nearesSystemDistance){
+          nearestSystems = [system1, system2];
+          nearesSystemDistance = distance;
         }
       }
     }
 
-    //Add random cycle
-    //const randomCycles = Math.randInt(count/10);
-    const randomCycles = 0;
-    for(let i = 0; i < randomCycles; i++) {
-      const system1 = systems[Math.randInt(systems.length)];
-      const system2 = systems[Math.randInt(systems.length)];
-
-      if(system1 != system2) this.connectSystems(system1, system2);
-    }
-
-    return systems;
+    this.connectSystems(nearestSystems[0], nearestSystems[1]);
   }
 
   /**
    * Checks if a system is part of a cycle in a cluster of systems
    * */
-  private checkForCycles(systems: System[], start: System): boolean {
-    const systemsCopy: System[] = [];
-    for(const system of systems) {
-      const copy = new System();
-      copy._id = system._id;
-      copy.links = {...system.links};
-      systemsCopy.push(copy);
-    }
+  private hasCycle(start: number, edges: number[][]): boolean {
+    const visited: number[][] = edges.filter(edge => edge.includes(start));
+    const stack: number[] = visited.map(edge => edge[0] === start ? edge[1] : edge[0]);
 
-    const startCopy = new System();
-    startCopy._id = start._id;
-    startCopy.links = {...start.links};
-
-    const stack:System[] = [];
-
-    for(const link of Object.keys(startCopy.links)) {
-      const neighbors = systemsCopy.filter(system => system._id.toString() === link);
-      neighbors.forEach(neighbor => this.removeConnection(startCopy, neighbor));
-      stack.push(...neighbors);
-    }
-
-    while(stack.length > 0) {
+    while (stack.length > 0) {
       const current = stack.pop();
       if(!current) break;
 
-      if(current == start) return true;
-
-      for(const link in current.links) {
-        const neighbors = systemsCopy.filter(system => system._id.toString() === link);
-        neighbors.forEach(neighbor => this.removeConnection(current, neighbor));
-        stack.push(...neighbors);
+      for(const edge of edges.filter(edge => !visited.includes(edge))) {
+        if(edge[0] === current) {
+          if(edge[1] === start) return true;
+          visited.push(edge);
+          stack.push(edge[1]);
+        }
+        else if(edge[1] === current) {
+          if(edge[0] === start) return true;
+          visited.push(edge);
+          stack.push(edge[0]);
+        }
       }
     }
+
     return false;
   }
 
-  private distanceSortedSystems(systems: System[], system: System): System[] {
-    return systems.sort((a, b) => {
-      const distanceA = Math.hypot(a.x - system.x, a.y - system.y);
-      const distanceB = Math.hypot(b.x - system.x, b.y - system.y);
-      return distanceA - distanceB;
-    }).filter(s => s != system);
+  private hasIntersection(grid: Grid, edges: number[][], newEdge: number[]): boolean {
+    return edges.some(edge => this.isIntersecting(grid, edge, newEdge) || this.isIntersecting(grid, newEdge, edge));
   }
 
-  private edgesSortedSystems(systems: System[]): System[] {
-    return systems.sort((a, b) => Object.keys(a.links).length - Object.keys(b.links).length);
+  private isIntersecting(grid: Grid, edge1: number[], edge2: number[]): boolean {
+    for(const intersectingEdges of grid.intersecting_edges) {
+      if((intersectingEdges[0][0] == edge1[0] && intersectingEdges[0][1] == edge1[1]
+        && intersectingEdges[1][0] == edge2[0] && intersectingEdges[1][1] == edge2[1])) return true;
+    }
+
+    return false;
   }
 
   private connectSystems(system1: System, system2: System): void {
@@ -263,22 +349,18 @@ export class SystemService extends MongooseRepository<System> {
     system2.links[system1._id.toString()] = distance;
   }
 
-  private removeConnection(system1: System, system2: System): void {
-    delete system1.links[system2._id.toString()];
-    delete system2.links[system1._id.toString()];
-  }
-
-  private createSystem(game: Game, x: number, y: number): System {
+  private createSystem(game: Game, vertex: Vertex, scaling: number, offset: number[]): System {
     const systemType = Object.entries(SYSTEM_TYPES).randomWeighted(([, value]) => value.chance)[0] as SystemTypeName;
     const capacity_range = SYSTEM_TYPES[systemType].capacity_range;
 
     return {
       _id: new Types.ObjectId(),
       game: game._id,
+      owner: game.owner,
       type: systemType,
       capacity: Math.randInt(capacity_range[1] - capacity_range[0]) + capacity_range[0],
-      x: x,
-      y: y,
+      x: vertex.x * scaling + offset[0] + Math.random() * scaling * MAX_SYSTEM_DISPLACEMENT,
+      y: vertex.y * scaling + offset[1] + Math.random() * scaling * MAX_SYSTEM_DISPLACEMENT,
       upgrade: SYSTEM_UPGRADE_NAMES[0],
       links: {},
       districtSlots: {},
