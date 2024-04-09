9ef183cf767add4420c3748a73f4d570753637c7
Adrian Kunz <a.kunz@uni-kassel.de>
2024-04-09 15:53:01 +0200

    fix: Missing MemberModule import in GameLogicModule

--- src/game-logic/game-logic.module.ts
+++ src/game-logic/game-logic.module.ts
@@ -7,10 +7,12 @@ import {SystemModule} from '../system/system.module';
 import {EmpireModule} from '../empire/empire.module';
 import {GameLogicController} from './game-logic.controller';
 import {GameLogicHandler} from './game-logic.handler';
+import {MemberModule} from '../member/member.module';
 
 @Module({
   imports: [
     GameModule,
+    MemberModule,
     SystemModule,
     EmpireModule,
   ],
