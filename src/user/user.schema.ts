f26ad91a45a8b8613bcd57a724ba8adc918c5de3
Adrian Kunz <a.kunz@uni-kassel.de>
2024-04-29 14:30:38 +0200

    feat: Re-add User avatar

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,9 +1,11 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty, IsString, MaxLength} from 'class-validator';
+import {IsByteLength, IsDataURI, IsNotEmpty, IsOptional, IsString, MaxLength} from 'class-validator';
 import {Document, Types} from 'mongoose';
 import {GLOBAL_SCHEMA_OPTIONS, GlobalSchema} from '../util/schema';
 import {TECHNOLOGIES} from "../game-logic/technologies";
+
+export const MAX_AVATAR_LENGTH = 16 * 1024;
 
 @Schema(GLOBAL_SCHEMA_OPTIONS)
 export class User extends GlobalSchema {
@@ -13,6 +15,23 @@ export class User extends GlobalSchema {
   @IsNotEmpty()
   @MaxLength(32)
   name: string;
+
+  @Prop()
+  @IsOptional()
+  @IsDataURI()
+  @IsByteLength(0, MAX_AVATAR_LENGTH)
+  @ApiProperty({
+    description: 'Data URI with a base64-encoded image',
+    externalDocs: {
+      description: 'Data URI scheme - Wikipedia',
+      url: 'https://en.wikipedia.org/wiki/Data_URI_scheme',
+    },
+    example: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
+    format: 'uri',
+    required: false,
+    maxLength: MAX_AVATAR_LENGTH,
+  })
+  avatar?: string;
 
   @Prop({transform: () => undefined})
   passwordHash: string;
