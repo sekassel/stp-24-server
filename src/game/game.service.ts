82f5f6701a6be11d33463daca4a28c64fcae1bbc
Adrian Kunz <clashsoft@hotmail.com>
2024-05-18 14:08:58 +0200

    fix: Update game password

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
-import {Model} from 'mongoose';
+import {Model, Types, UpdateQuery} from 'mongoose';
 import {CreateGameDto, UpdateGameDto} from './game.dto';
 import {Game, GameDocument} from './game.schema';
 import {EventRepository, EventService, MongooseRepository} from '@mean-stream/nestx';
@@ -17,18 +17,23 @@ export class GameService extends MongooseRepository<Game> {
     super(model);
   }
 
-  private async hash(dto: UpdateGameDto) {
-    const {password, ...rest} = dto;
-    const result: Partial<Game> = rest;
-    if (password) {
+  private async hash<T extends object>(dto: T): Promise<Omit<T, 'password'> & {passwordHash?: string}> {
+    if ('password' in dto) {
       const passwordSalt = await bcrypt.genSalt();
-      result.passwordHash = await bcrypt.hash(password, passwordSalt);
+      const passwordHash = await bcrypt.hash(dto.password as string, passwordSalt);
+      const result = {...dto, passwordHash};
+      delete result.password;
+      return result;
     }
-    return result;
+    return dto;
   }
 
   async create(game: CreateGameDto | Omit<Game, keyof GlobalSchema>): Promise<GameDocument> {
-    return super.create((await this.hash(game)) as Game);
+    return super.create((await this.hash(game)) as Omit<Game, keyof GlobalSchema>);
+  }
+
+  async update(id: Types.ObjectId, game: UpdateGameDto | UpdateQuery<Game>): Promise<GameDocument | null> {
+    return super.update(id, await this.hash(game));
   }
 
   emit(event: string, game: Game): void {
