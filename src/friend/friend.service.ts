6a6e7ee54acbf81b3c5edb17a2820a1ad68e531c
simonloeser <92864888+simonloeser@users.noreply.github.com>
2024-06-04 14:31:48 +0200

    fix: Delete unused friend.service methods

--- src/friend/friend.service.ts
+++ src/friend/friend.service.ts
@@ -1,4 +1,4 @@
-import {ConflictException, Injectable, NotFoundException} from "@nestjs/common";
+import {Injectable} from "@nestjs/common";
 import {InjectModel} from "@nestjs/mongoose";
 import {Friend, FriendDocument} from "./friend.schema";
 import {Model, Types} from "mongoose";
@@ -21,38 +21,10 @@ export class FriendsService extends MongooseRepository<Friend> {
     return this.friendModel.find({from, status}).exec();
   }
 
-  async createFriendRequest(from: Types.ObjectId, to: Types.ObjectId): Promise<FriendDocument> {
-    const existingRequest = await this.friendModel.findOne({from: to, to: from}).exec();
-    if (existingRequest) {
-      throw new ConflictException('You already have a friend request from this user.');
-    }
-
-    const existingFriend = await this.friendModel.findOne({from, to}).exec();
-    if (existingFriend) {
-      throw new ConflictException('Friend request already exists.');
-    }
-
-    const friend = new this.friendModel({from, to, status: 'requested'});
-    return friend.save();
-  }
-
   async acceptFriendRequest(to: Types.ObjectId, from: Types.ObjectId): Promise<FriendDocument | null> {
     await this.updateOne({from, to, status: 'requested'}, {status: 'accepted'}, {new: true});
     const inverseFriend = new this.friendModel({from: to, to: from, status: 'accepted'});
     return inverseFriend.save();
-  }
-
-  async deleteFriend(from: Types.ObjectId, to: Types.ObjectId): Promise<FriendDocument> {
-    const friend = await this.friendModel.findOneAndDelete({from, to}).exec();
-    if (!friend) {
-      throw new NotFoundException('Friend not found.');
-    }
-    await this.friendModel.findOneAndDelete({from: to, to: from}).exec();
-    return friend;
-  }
-
-  async deleteAllFriendsForUser(userId: Types.ObjectId): Promise<void> {
-    await this.friendModel.deleteMany({$or: [{from: userId}, {to: userId}]}).exec();
   }
 
   private emit(event: string, friend: Friend) {
