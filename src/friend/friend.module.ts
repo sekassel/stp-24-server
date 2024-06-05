72ab6471e14a4d1755134d183becd90dcdc359f4
simonloeser <92864888+simonloeser@users.noreply.github.com>
2024-06-05 11:31:53 +0200

    fix: Delete user event handling

--- src/friend/friend.module.ts
+++ src/friend/friend.module.ts
@@ -3,10 +3,13 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {Friend, FriendSchema} from "./friend.schema";
 import {FriendsService} from "./friend.service";
 import {FriendsController} from "./friend.controller";
+import {FriendsHandler} from "./friend.handler";
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: Friend.name, schema: FriendSchema}])],
-  providers: [FriendsService],
+  imports: [
+    MongooseModule.forFeature([{name: Friend.name, schema: FriendSchema}]),
+  ],
+  providers: [FriendsService, FriendsHandler],
   controllers: [FriendsController],
   exports: [FriendsService],
 })
