21dba2a67495bc8ea0b01d1004ec3d099591137b
Adrian Kunz <a.kunz@uni-kassel.de>
2024-06-20 11:23:15 +0200

    fix: Pause all games by owner when a new game is created

--- src/game/game.handler.ts
+++ src/game/game.handler.ts
@@ -2,12 +2,21 @@ import {Injectable} from '@nestjs/common';
 import {OnEvent} from '@nestjs/event-emitter';
 import {User} from '../user/user.schema';
 import {GameService} from './game.service';
+import {Game} from './game.schema';
 
 @Injectable()
 export class GameHandler {
   constructor(
     private gameService: GameService,
   ) {
+  }
+
+  /**
+   * When a game is created, pause all the other games owner by the same user.
+   */
+  @OnEvent('games.*.created')
+  async onGameCreated(game: Game): Promise<void> {
+    await this.gameService.updateMany({owner: game.owner}, {speed: 0});
   }
 
   @OnEvent('users.*.deleted')
