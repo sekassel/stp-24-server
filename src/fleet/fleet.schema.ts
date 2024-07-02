be6314e2014c2ecd20a9c2900ff36b969849d2ea
simonloeser <92864888+simonloeser@users.noreply.github.com>
2024-07-02 15:50:44 +0200

    feat: Init fleet schemas

---
+++ src/fleet/fleet.schema.ts
@@ -0,0 +1,57 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document, Types} from 'mongoose';
+import {GLOBAL_SCHEMA_OPTIONS, GlobalSchema} from '../util/schema';
+import {IsObject, IsOptional, IsString, ValidateNested} from 'class-validator';
+import {ApiProperty, ApiPropertyOptional} from '@nestjs/swagger';
+import {AsObjectId, Ref} from '@mean-stream/nestx';
+import {EffectSource} from "../game-logic/types";
+import {ShipType} from "./ship-type.schema";
+
+export type FleetDocument = Fleet & Document<Types.ObjectId>;
+
+@Schema(GLOBAL_SCHEMA_OPTIONS)
+export class Fleet extends GlobalSchema {
+  @Ref('Game')
+  game: Types.ObjectId;
+
+  @Ref('Empire')
+  @ApiPropertyOptional({description: 'Owner Empire ID, or undefined if not owned by anyone (wild fleets).'})
+  @AsObjectId()
+  @IsOptional()
+  empire?: Types.ObjectId;
+
+  @Prop({required: true})
+  @ApiProperty({description: 'Custom name of the fleet.'})
+  @IsString()
+  name: string;
+
+  @Prop({required: true})
+  @ApiProperty({description: 'ID of the system the fleet is currently stationed at.'})
+  @AsObjectId()
+  location: Types.ObjectId;
+
+  @Prop({type: Map, of: Number})
+  @ApiProperty({description: 'Number of ships within this fleet if fully built.'})
+  @IsObject()
+  size: Record<ShipType, number>;
+
+  @Prop({type: Object, default: {}})
+  @ApiPropertyOptional({description: 'Custom data, visible only to the owner empire.'})
+  @IsObject()
+  @IsOptional()
+  _private?: Record<string, any>;
+
+  @Prop({type: Object, default: {}})
+  @ApiPropertyOptional({description: 'Custom data, visible to everyone.'})
+  @IsObject()
+  @IsOptional()
+  _public?: Record<string, any>;
+
+  @Prop({type: [Object], default: []})
+  @ApiPropertyOptional({description: 'Custom effects.'})
+  @ValidateNested({each: true})
+  @IsOptional()
+  effects?: EffectSource[];
+}
+
+export const FleetSchema = SchemaFactory.createForClass(Fleet);
