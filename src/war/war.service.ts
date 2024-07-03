6ec214e5ae7efba6e63d70ebe223626a828744d1
Simon Löser <92864888+simonloeser@users.noreply.github.com>
2024-07-03 10:56:41 +0200

    Wars (#91)
    * feat: Add war schema and dtos
    
    * feat: Add war handler
    
    * feat: Add war controller
    
    * fix: Get war empire filtering
    
    * docs: Add war documentation, probably bs
    
    * refactor: War controller and dtos
    
    * feat: Don't create already existing wars
    
    * fix: War conflict response

---
+++ src/war/war.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@nestjs/common";
+import {EventRepository, EventService, MongooseRepository} from "@mean-stream/nestx";
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {War} from "./war.schema";
+
+@Injectable()
+@EventRepository()
+export class WarService extends MongooseRepository<War> {
+  constructor(
+    @InjectModel(War.name) model: Model<War>,
+    private eventEmitter: EventService,
+  ) {
+    super(model);
+  }
+
+  emit(event: string, war: War) {
+    this.eventEmitter.emit(`games.${war.game}.wars.${war._id}.${event}`, war);
+  }
+}
