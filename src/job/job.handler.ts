e79b7147ea045b487a370960a3a2ffa96cb856c3
Adrian Kunz <a.kunz@uni-kassel.de>
2024-08-08 16:16:47 +0200

    fix: Delete jobs on empire deletion

--- src/job/job.handler.ts
+++ src/job/job.handler.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {OnEvent} from '@nestjs/event-emitter';
 import {JobService} from './job.service';
 import {Game} from '../game/game.schema';
+import {Empire} from '../empire/empire.schema';
 
 @Injectable()
 export class JobHandler {
@@ -13,5 +14,10 @@ export class JobHandler {
   @OnEvent('games.*.deleted')
   async onGameDeleted(game: Game): Promise<void> {
     await this.jobService.deleteMany({game: game._id});
+  }
+
+  @OnEvent('games.*.empires.*.deleted')
+  async onEmpireDeleted(empire: Empire): Promise<void> {
+    await this.jobService.deleteMany({empire: empire._id});
   }
 }
