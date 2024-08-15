b5ca5b7d7375dd7351954740af9be77a69f0e44b
Adrian Kunz <a.kunz@uni-kassel.de>
2024-08-15 09:42:21 +0200

    fix: Delete travel jobs on fleet deletion

--- src/job/job.handler.ts
+++ src/job/job.handler.ts
@@ -3,6 +3,8 @@ import {OnEvent} from '@nestjs/event-emitter';
 import {JobService} from './job.service';
 import {Game} from '../game/game.schema';
 import {Empire} from '../empire/empire.schema';
+import {Fleet} from '../fleet/fleet.schema';
+import {JobType} from './job-type.enum';
 
 @Injectable()
 export class JobHandler {
@@ -19,5 +21,16 @@ export class JobHandler {
   @OnEvent('games.*.empires.*.deleted')
   async onEmpireDeleted(empire: Empire): Promise<void> {
     await this.jobService.deleteMany({empire: empire._id});
+  }
+
+  @OnEvent('games.*.fleets.*.deleted')
+  async onFleetDeleted(fleet: Fleet): Promise<void> {
+    // Cancel travel jobs when a fleet is deleted.
+    // We don't delete the ship jobs because they can complete without the fleet existing,
+    // and we would have to refund resources, which is not practical here.
+    await this.jobService.deleteMany({
+      type: JobType.TRAVEL,
+      fleet: fleet._id,
+    });
   }
 }
